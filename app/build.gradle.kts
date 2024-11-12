95c227b0056db467ace2e684eee3614ebd75883a
Rob Rogers <robrogers@zoominternet.net>
2024-11-12 16:45:13 -0500

    v16 release changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,15 +30,15 @@ android {
         applicationId = "com.immichframe.immichframe"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 16
+        versionName = "1.0.16.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
