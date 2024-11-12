6ba9cebb5957a0791802a4495a743ac685fed822
JW-CH <17313367+JW-CH@users.noreply.github.com>
2024-11-12 16:24:38 +0100

    Revert "remove the URL check"
    This reverts commit 0b7879713cbeac4e44e567bc0ab4c9fe8196cfa8.

--- app/src/main/java/com/immichframe/immichframe/SettingsActivity.kt
+++ app/src/main/java/com/immichframe/immichframe/SettingsActivity.kt
@@ -6,7 +6,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var editTextUrl: EditText
@@ -19,18 +25,41 @@ class SettingsActivity : AppCompatActivity() {
         editTextUrl = findViewById(R.id.editTextUrl)
         buttonSaveUrl = findViewById(R.id.buttonSaveUrl)
 
+        // Load saved URL
         loadSavedUrl()
 
         buttonSaveUrl.setOnClickListener {
             val url = editTextUrl.text.toString()
-                if (url.isNotEmpty()) {
+
+            // Use a coroutine to perform network request asynchronously
+            CoroutineScope(Dispatchers.Main).launch {
+                val isSuccessful = checkUrlGetResponse(url)
+                if (url.isNotEmpty() && isSuccessful) {
+                    // Save the URL
                     saveUrl(url)
                     Toast.makeText(this@SettingsActivity, "URL saved!", Toast.LENGTH_SHORT).show()
                     setResult(Activity.RESULT_OK)
-                    finish()
+                    finish() // Close the settings activity and return to the previous activity
                 } else {
                     Toast.makeText(this@SettingsActivity, "Your ImmichFrame URL is not valid!", Toast.LENGTH_SHORT).show()
                 }
+            }
+        }
+    }
+
+    private suspend fun checkUrlGetResponse(url: String): Boolean {
+        return withContext(Dispatchers.IO) { // Switch to IO context for network request
+            val client = OkHttpClient()
+            val request = Request.Builder().url("$url/api/Config").build()
+
+            try {
+                client.newCall(request).execute().use { response ->
+                    response.isSuccessful
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                false
+            }
         }
     }
 
