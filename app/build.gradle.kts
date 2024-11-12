9e19d2c878a8259a1cb7201785d077054dc11be7
JW-CH <17313367+JW-CH@users.noreply.github.com>
2024-11-12 16:24:59 +0100

    Revert "Add URL-Check to Save settings"
    This reverts commit 3a27143faba8f56202755873af7b3668252223cf.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.okhttp)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/immichframe/immichframe/SettingsActivity.kt
+++ app/src/main/java/com/immichframe/immichframe/SettingsActivity.kt
@@ -6,13 +6,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.io.IOException
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var editTextUrl: EditText
@@ -30,35 +23,12 @@ class SettingsActivity : AppCompatActivity() {
 
         buttonSaveUrl.setOnClickListener {
             val url = editTextUrl.text.toString()
-
-            // Use a coroutine to perform network request asynchronously
-            CoroutineScope(Dispatchers.Main).launch {
-                val isSuccessful = checkUrlGetResponse(url)
-                if (url.isNotEmpty() && isSuccessful) {
-                    // Save the URL
-                    saveUrl(url)
-                    Toast.makeText(this@SettingsActivity, "URL saved!", Toast.LENGTH_SHORT).show()
-                    setResult(Activity.RESULT_OK)
-                    finish() // Close the settings activity and return to the previous activity
-                } else {
-                    Toast.makeText(this@SettingsActivity, "Your ImmichFrame URL is not valid!", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    private suspend fun checkUrlGetResponse(url: String): Boolean {
-        return withContext(Dispatchers.IO) { // Switch to IO context for network request
-            val client = OkHttpClient()
-            val request = Request.Builder().url("$url/api/Config").build()
-
-            try {
-                client.newCall(request).execute().use { response ->
-                    response.isSuccessful
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-                false
+            if (url.isNotEmpty()) {
+                // Save the URL
+                saveUrl(url)
+                Toast.makeText(this, "URL saved!", Toast.LENGTH_SHORT).show()
+                setResult(Activity.RESULT_OK)
+                finish() // Close the settings activity and return to the previous activity
             }
         }
     }
